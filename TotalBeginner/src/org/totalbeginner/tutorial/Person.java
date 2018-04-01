4372da2596798c81259b4e88a644c1873cd43fea
Sudeshana Ariyasena <dgpradeepal@gmail.com>
2018-04-01 18:07:07 +0530

    first commit

---
+++ TotalBeginner/src/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,12 @@
+package org.totalbeginner.tutorial;
+
+public class Person {
+	
+	private String name;
+	private int maximumBooks;
+	
+	public Person() {
+		name = "unknown name";
+		maximumBooks = 3;
+	}
+}
